26a1690d8b4e030bb03108fd5313180e97c5a50e
hipo760 <hipo760@outlook.com>
2019-10-27 04:08:37 +0800

    Update the unit-test script.

--- unit-test.ps1
+++ unit-test.ps1
@@ -1,3 +1,3 @@
-.\packages\xunit.runner.console.2.4.1\tools\net472\xunit.console.exe `
-.\Tests\QuoteService.Schedule.Tests\bin\Release\QuoteService.Schedule.Tests.dll `
-.\Tests\SKAPI.Tests\bin\Release\SKAPI.Tests.dll
+packages\xunit.runner.console.2.4.1\tools\net472\xunit.console.exe `
+Tests\QuoteService.Schedule.Tests\bin\Release\QuoteService.Schedule.Tests.dll `
+Tests\SKAPI.Tests\bin\Release\SKAPI.Tests.dll
